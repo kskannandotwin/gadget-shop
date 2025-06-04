13b05efaaafca060aa55fd7ff4bf5d773c4ed560
kannanATexcelforte <kannan@excelforte.com>
2025-06-04 08:14:50 +0530

    04.06.2025
    four has completed

--- src/app/AppComponents/inventory/inventory.component.ts
+++ src/app/AppComponents/inventory/inventory.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inventory',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css'
 })
 export class InventoryComponent {
 
+  inventoryData = {
+    productId: '',
+    productName: '',
+    availableQty: 0,
+    reorderPoint: 0
+  }
+  onSubmit() {
+    alert('Form submitted successfully! ' + JSON.stringify(this.inventoryData));
+  }
 }
